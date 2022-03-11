2de3e81ae7f6f41212425114343da9bd3d90a73f
Joël Piguet <aurore.azure@gmail.com>
2022-03-11 15:07:23 +0100

    220311

---
+++ _util/privatesettings_template.php
@@ -0,0 +1,19 @@
+<?php
+
+declare(strict_types=1);
+
+################################
+## Joël Piguet - 2021.12.15 ###
+##############################
+// Private setting not to be stored in github.
+
+// database
+const MYSQL_HOST = '';
+const MYSQL_PORT = 0000;
+const MYSQL_SCHEMA = 'heds_inv_exp'; # HEdS Inventory expiration
+const MYSQL_ADMIN_ID = '';
+const MYSQL_ADMIN_PASSWORD = '';
+
+// email account
+const APP_EMAIL = '';
+const APP_EMAIL_PASSWORD = '';
